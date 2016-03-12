5b0a97f7587c621b18bb3cbc851fd40052ebfd0f
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-03-12 18:28:24 +0100

    [ImagesEndpoint] Upload first error handling

--- application/controllers/Images.php
+++ application/controllers/Images.php
@@ -2,6 +2,14 @@
 
 require APPPATH . 'libraries/REST_Controller.php';
 
+class ImagesResponse {
+    const ERROR_FILESIZE_LIMIT_EXCEEDED = "Exceeded filesize limit.";
+    const ERROR_NO_FILE_SENT = "No file sent.";
+    const ERROR_INVALID_FILE_FORMAT = "Invalid file format.";
+    const ERROR_UNKNOWN = "Unknown error.";
+    const SUCCESS_IMAGE_UPLOADED = "Image uploaded correctly.";
+}
+
 class Images extends REST_Controller {
 
     function get_get($id = NULL) {
@@ -9,9 +17,51 @@ class Images extends REST_Controller {
     }
 
     function upload_post($id = NULL) {
-        print_r($_FILES);
+        $file = $_FILES['file'];
+        if ($file['error'] == UPLOAD_ERR_OK) {
 
-        $this->response(["files" => []], REST_Controller::HTTP_OK);
+            if ($file['size'] > 1000000) {
+                $this->throwError(ImagesResponse::ERROR_FILESIZE_LIMIT_EXCEEDED);
+                die();
+            }
+
+            $file_info = new finfo(FILEINFO_MIME_TYPE);
+            if (false === $ext = array_search(
+                    $file_info->file($file['tmp_name']),
+                    array(
+                        'jpg' => 'image/jpeg',
+                        'png' => 'image/png',
+                        'gif' => 'image/gif',
+                    ),
+                    true
+                )) {
+                $this->throwError(ImagesResponse::ERROR_INVALID_FILE_FORMAT);
+            }
+
+            $this->success(ImagesResponse::SUCCESS_IMAGE_UPLOADED);
+        } else {
+            $error = '';
+            switch ($file['error']) {
+                case UPLOAD_ERR_NO_FILE:
+                    $error = ImagesResponse::ERROR_NO_FILE_SENT;
+                    break;
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    $error = ImagesResponse::ERROR_FILESIZE_LIMIT_EXCEEDED;
+                    break;
+                default:
+                    $error = ImagesResponse::ERROR_UNKNOWN;
+                    break;
+            }
+            $this->throwError($error);
+        }
     }
 
+    private function success($success) {
+        $this->response(["Success" => $success], REST_Controller::HTTP_OK);
+    }
+
+    private function throwError($error) {
+        $this->response(["error" => $error], REST_Controller::HTTP_BAD_REQUEST);
+    }
 }
