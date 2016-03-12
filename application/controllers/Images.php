47ccf6319515eb00e6268bea1f36714b1873c827
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-03-12 16:24:46 +0100

    [ImagesEndpoint] Merged UploadImages & Images controller to one

--- application/controllers/Images.php
+++ application/controllers/Images.php
@@ -4,8 +4,12 @@ require APPPATH . 'libraries/REST_Controller.php';
 
 class Images extends REST_Controller {
 
-    function index_get($id = NULL) {
-        $this->response(["images" => []], REST_Controller::HTTP_OK);
+    function get_get($id = NULL) {
+        $this->response(["images" => [$id]], REST_Controller::HTTP_OK);
+    }
+
+    function upload_get($id = NULL) {
+        $this->response(["images" => [$id]], REST_Controller::HTTP_OK);
     }
 
 }
