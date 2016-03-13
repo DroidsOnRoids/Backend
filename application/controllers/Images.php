41335f0daa39fa29ca3e68c488cb23ddbf99402d
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-03-13 13:39:08 +0100

    [ImagesEndpoint] Add hour to the file name

--- application/controllers/Images.php
+++ application/controllers/Images.php
@@ -102,11 +102,12 @@ class Images extends REST_Controller {
             }
 
             $path = $this->getPathForUser($to_userId);
-            $file_name = sprintf('%s%s_%s_%s.%s',
+            $file_name = sprintf('%s%s_%s_%s_%s.%s',
                 $path,
                 $from_userId,
                 date('Y.m.d'),
-                sha1_file($file['tmp_name'].date('H:i:s')),
+                date('H.i.s'),
+                sha1_file($file['tmp_name']),
                 $ext
             );
             if (!move_uploaded_file($file['tmp_name'], $file_name)) {
