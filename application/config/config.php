b6431b55764bff0b3fc3902381dfb92303b72aa7
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-03-13 20:22:42 +0100

    [General] Add https support for image urls

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://serene-escarpment-58247.herokuapp.com/';
+$config['base_url'] = 'https://serene-escarpment-58247.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
